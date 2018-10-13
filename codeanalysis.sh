aecee1ba77eb2d20c948f6635a000e75729924ae
Alok Shukla <32108973+alokshukla1978@users.noreply.github.com>
2018-10-13 15:51:18 -0700

    Update codeanalysis.sh

--- codeanalysis.sh
+++ codeanalysis.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "Hello World"
+curl -O https://www.shiftleft.io/download/sl-latest-windows-x64.zip
 echo "AGENT_WORKFOLDER is $AGENT_WORKFOLDER"
 echo "AGENT_WORKFOLDER contents:"
 ls -1 $AGENT_WORKFOLDER
