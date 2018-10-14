421465fb8d5981f1f709cee779802ff41fbcb59c
Alok Shukla <32108973+alokshukla1978@users.noreply.github.com>
2018-10-14 09:24:28 -0700

    Update codeanalysis.sh

--- codeanalysis.sh
+++ codeanalysis.sh
@@ -5,10 +5,14 @@ echo "Extracting Shiftleft Agent!"
 powershell -Command "& {Expand-Archive sl-latest-windows-x64.zip -DestinationPath . ;}"
 echo "Setting SL authentication parameters!"
 $BUILD_SOURCESDIRECTORY\\sl auth --org $ORGID --token $TOKENID
-echo %DATE%
-echo %TIME%
-set datetimef=%date:~-4%_%date:~3,2%_%date:~0,2%__%time:~0,2%_%time:~3,2%_%time:~6,2%
-echo %datetimef%
+set month=%date:~4,2%
+if "%month:~0,1%" == " " set month=0%month:~1,1%
+echo month=%month%
+set day=%date:~0,2%
+if "%day:~0,1%" == " " set day=0%day:~1,1%
+echo day=%day%
+set datetimef=%year%%month%%day%_%hour%%min%%secs%
+echo datetimef=%datetimef%
 sleep 10
 echo "Performing Code Analysis!"
 $BUILD_SOURCESDIRECTORY\\sl analyze --app Hello-ShiftLeft-DevOps --cpg --csharp --dotnet-framework --csharp2cpg-args "-l info" "$BUILD_SOURCESDIRECTORY\\WebSite\WebSite.csproj"
